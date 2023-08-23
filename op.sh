f2c2a42058b1f61fb325f8a38c9bd3549c3db3d2
fummeys <fummeys@gmail.com>
2023-08-23 23:51:10 +0100

    update 1.01
    added uninstall
    added open session command

--- op.sh
+++ op.sh
@@ -4,4 +4,16 @@ projectDir="$(du ~ | fzf | awk '{print $2}')"
 
 projectName="$(echo $projectDir | awk --field-separator '/' '{print $NF}')"
 
-tmux new-window -c $projectDir -n $projectName
+openWindow(){
+	tmux new-window -c $projectDir -n $projectName
+}
+
+openSession(){
+	tmux new-session -c $projectDir -n $projectName
+}
+
+openWindow
+
+if [[ $? == 1 ]]; then
+	openSession
+fi
---
+++ uninstall.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo rm /bin/op
+
+sudo rm -rfv /opt/open-project
