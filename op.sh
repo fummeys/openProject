43043c96308aace389b6c3f3bc6cf55373a4d6a7
Samuel Fummey <fummeys@gmail.com>
2024-07-26 12:49:21 +0100

    fix: check if tmux session is already attached

--- op.sh
+++ op.sh
@@ -2,8 +2,6 @@
 
 type=$1
 
-echo $type
-
 source ~/.op/oprc
 
 projectDir="$(du $projectDirs | fzf | awk '{print $2}')"
@@ -11,18 +9,24 @@ projectDir="$(du $projectDirs | fzf | awk '{print $2}')"
 projectName="$(echo $projectDir | awk --field-separator '/' '{print $NF}')"
 
 openWindow(){
-	tmux new-window -c $projectDir -n $projectName
+    if [[ $TERM_PROGRAM = "tmux" ]]; then
+        tmux new-window -c $projectDir -n $projectName
+    else
+        tmux
+        tmux new-window -c $projectDir -n $projectName
+    fi
 }
 
 openSession(){
-	tmux switch-client -t `tmux new-session -dP -c $projectDir -n $projectName -s $projectName`
+    if [[ $TERM_PROGRAM = "tmux" ]]; then
+        tmux switch-client -t `tmux new-session -dP -c $projectDir -n $projectName`
+    else
+        tmux attach -t `tmux new-session -dP -c $projectDir -n $projectName`
+    fi
 }
-
 
 if [[ $type == "n" ]]; then
     openWindow
 else
     openSession
 fi
-
-
